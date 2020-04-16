ef8a6df5854100ea8c80bed1c19754a25bea987f
Emre <kutukemre68@gmail.com>
2020-04-16 15:52:48 +0200

    4th term 1st week

---
+++ Programming4/Lab1/Lab1/Assignment 3/Pencil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Pencil : IPencil
+    {
+        private Char[] charArray;
+        private const int MAXTOWRITE = 20;
+        private int nrOfCharsWritten;
+        public bool CanWrite { get { if (nrOfCharsWritten >= MAXTOWRITE ) return false; else return true; } }
+
+        public void AfterSharpening()
+        {
+            this.nrOfCharsWritten = 0;
+        }
+
+        public void Write(string message)
+        {
+            if(message == "sharpen")
+            {
+                PencilSharpener sharpener = new PencilSharpener();
+                sharpener.Sharpen(this);
+            }
+            else
+            { 
+                charArray = message.ToCharArray();
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach(char c in charArray)
+                {
+                    if (CanWrite)
+                    { 
+                        Console.Write(c);
+                        nrOfCharsWritten++;
+                    }
+                    else
+                        Console.Write("#");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Programming4/Lab1/Lab1/Assignment 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+
+        void Start()
+        {
+            Pencil pencil = new Pencil();
+            Console.Write("Enter a word : ");
+            string userInput = Console.ReadLine();
+
+            while(userInput != "stop")
+            { 
+                pencil.Write(userInput);
+                Console.Write("\nEnter a word : ");
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nend of program");
+            Console.ReadKey();
+        }
+    }
+}
